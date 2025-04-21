1186d0a58bfb22ff7c12d8bc2ba24cd37c5a9dfa
missmel75 <mcartagena6@hawkmail.hccfl.edu>
2025-04-20 21:16:59 -0400

    Create main04.py

---
+++ main04.py
@@ -0,0 +1,63 @@
+#AutoCountry Car Finder
+#initialize vehicle list
+allowed_vehicles = [
+        'Ford F-150',
+        'Chevrolet Silverado',
+        'Tesla CyberTruck',
+        'Toyota Tundra',
+        'Nissan Titan'
+    ]
+
+#display menu
+print("********************************")
+print("AutoCountry Vehicle Finder v0.2")  
+print("********************************")
+print("Please Enter the following number below from the following menu:")
+print(" ")
+print("1. PRINT all Authorized Vehicles")
+print("2. SEARCH for Authorized Vehicle") 
+print("3. Exit") 
+print(" ")
+
+#user input
+running = True
+while running:
+    userChoice = input("Enter your choice: ")
+
+    if userChoice == "1":
+        print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+        print(" ")
+        for vehicle in allowed_vehicles:
+            print(vehicle)
+        print(" ")
+        print("********************************")
+        print("AutoCountry Vehicle Finder v0.2")  
+        print("********************************")
+        print("Please Enter the following number below from the following menu:")
+        print(" ")
+        print("1. PRINT all Authorized Vehicles")
+        print("2. SEARCH for Authorized Vehicle") 
+        print("3. Exit")  
+        print(" ")
+
+    elif userChoice == "2":  
+        vehicle_name = input("Please Enter the full Vehicle name: ")
+        if vehicle_name in allowed_vehicles:
+            print(f"{vehicle_name} is an authorized vehicle")
+        else:
+            print(f"{vehicle_name} is not an authorized vehicle, if you received this in error please check the spelling and try again")
+        print(" ")
+        print("********************************")
+        print("AutoCountry Vehicle Finder v0.2")
+        print("********************************")
+        print("Please Enter the following number below from the following menu:")
+        print(" ")
+        print("1. PRINT all Authorized Vehicles")
+        print("2. SEARCH for Authorized Vehicle")
+        print("3. Exit")
+        print(" ")
+
+    elif userChoice == "3":  
+        print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+        print(" ")
+        running = False
